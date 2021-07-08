5825483fdb59be6b4444f5ad14f2f2b9fc46ecf0
Antonio Contreras <drancope@gmail.com>
2021-07-08 13:01:15 +0200

    instrucciones básicas en Python

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hola")
+% Esto es un programa en lenguaje Python.
+% Las tres primeras líneas son comentarios, solo sirven para informar 
+% al lector, pero el intérprete de Python se las salta sin hacerles caso
+
+print("Hola") % esta orden imprime un mensaje en pantalla
+
+mi_dato = 5
+print(mi_dato) % aquí guardamos un dato en una variable y se muestra.
+
+dato = "Las variables pueden contener distintos tipos de datos"
+print(dato)
+
+mi_dato = dato
+print(dato) % el contenido de una variable puede cambiarse
+
+a = 4
+b = 6
+print(a+b)
+
